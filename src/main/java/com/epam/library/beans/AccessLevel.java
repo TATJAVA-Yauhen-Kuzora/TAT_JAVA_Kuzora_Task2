11df0f5a3a4cf6619415f2297d5ba1174d8a37a0
Yauhen Kuzora <Yauhen_Kuzora@epam.com>
2017-07-07 14:35:45 +0300

    add  javadocs

--- src/main/java/com/epam/library/beans/AccessLevel.java
+++ src/main/java/com/epam/library/beans/AccessLevel.java
@@ -1,28 +1,57 @@
-/**
- * 
- */
 package com.epam.library.beans;
 
 /**
- * @author Eugene13
- *
+ * Beans Class {@link AccessLevel}.
+ * <P>
+ * Class AccessLevel includes 2 fields (accessLevelId, accessLevel).
+ * <P>
+ * <i>This class is a member of the {@link com.epam.library.beans} package.</i>
  */
 public class AccessLevel {
+	/**
+	 * Contains access_level_id from library.access_level table.
+	 */
 	private int accessLevelId;
+	/**
+	 * Contains access_level from library.access_level table. Description for
+	 * access_level.
+	 */
 	private String accessLevel;
 
+	/**
+	 * Getter getAccessLevelId.
+	 * 
+	 * @return accessLevelId
+	 */
 	public int getAccessLevelId() {
 		return accessLevelId;
 	}
 
+	/**
+	 * Setter setAccessLevelId.
+	 * 
+	 * @param accessLevelId
+	 *            the accessLevelIds to set
+	 */
 	public void setAccessLevelId(int accessLevelId) {
 		this.accessLevelId = accessLevelId;
 	}
 
+	/**
+	 * Getter getName.
+	 * 
+	 * @return accessLevel
+	 */
 	public String getName() {
 		return accessLevel;
 	}
 
+	/**
+	 * Setter setName.
+	 * 
+	 * @param name
+	 *            the name to set
+	 */
 	public void setName(String name) {
 		this.accessLevel = name;
 	}
--- src/main/java/com/epam/library/beans/Book.java
+++ src/main/java/com/epam/library/beans/Book.java
@@ -1,50 +1,100 @@
-/**
- * 
- */
 package com.epam.library.beans;
 
 /**
- * @author Eugene13
- *
+ * Beans Class {@link Book}.
+ * <P>
+ * Class Book includes 4 fields (bookId, bookName, author, bookStatus).
+ * <P>
+ * <i>This class is a member of the {@link com.epam.library.beans} package.</i>
  */
 public class Book {
+	/**
+	 * Contains book_id from library.book table.
+	 */
 	private int bookId;
+	/**
+	 * Contains book_name from library.book table.
+	 */
 	private String bookName;
+	/**
+	 * Contains author from library.book table.
+	 */
 	private String author;
+	/**
+	 * Contains {@link BookStatus} Object as summary info about book available
+	 * property.
+	 */
 	private BookStatus bookStatus;
 
+	/**
+	 * Getter getBookId.
+	 * 
+	 * @return bookId
+	 */
 	public int getBookId() {
 		return bookId;
 	}
 
+	/**
+	 * Setter setBookId.
+	 * 
+	 * @param bookId
+	 *            the bookId to set
+	 */
 	public void setBookId(int bookId) {
 		this.bookId = bookId;
 	}
 
+	/**
+	 * Getter getBookName.
+	 * 
+	 * @return bookName
+	 */
 	public String getBookName() {
 		return bookName;
 	}
 
+	/**
+	 * Setter setBookName.
+	 * 
+	 * @param bookName
+	 *            the bookName to set
+	 */
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
 
+	/**
+	 * Getter getAuthore.
+	 * 
+	 * @return author
+	 */
 	public String getAuthor() {
 		return author;
 	}
 
+	/**
+	 * Setter setAuthor.
+	 * 
+	 * @param author
+	 *            the author to set
+	 */
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
 	/**
-	 * @return the bookStatus
+	 * Getter getBookStatus.
+	 * 
+	 * @return getBookStatus
 	 */
 	public BookStatus getBookStatus() {
 		return bookStatus;
 	}
 
 	/**
+	 * Setter setBookStatus.
+	 * 
 	 * @param bookStatus
 	 *            the bookStatus to set
 	 */
