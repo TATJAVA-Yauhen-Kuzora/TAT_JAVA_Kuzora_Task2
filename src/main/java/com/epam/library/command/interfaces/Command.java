f0a6b77d46f2c59ca46064e7413294b93d2d317d
Yauhen Kuzora <Yauhen_Kuzora@epam.com>
2017-07-08 22:56:11 +0300

    add javadocs for command.intefaces package

--- src/main/java/com/epam/library/command/interfaces/Command.java
+++ src/main/java/com/epam/library/command/interfaces/Command.java
@@ -5,11 +5,20 @@ import com.epam.library.command.exception.CommandException;
 /**
  * Interface {@link Command}.
  * <P>
- * Interface Command includes method {@link #execute(String...)}
+ * Interface Command includes method {@link #execute(String...)}.
  * <P>
- * <i>This Class is a member of the {@link com.epam.library.command.interfaces}
- * package.</i>
+ * <i>This interface is a member of the
+ * {@link com.epam.library.command.interfaces} package.</i>
  */
 public interface Command {
+	/**
+	 * Method execute.
+	 * 
+	 * @param request
+	 *            as array of String's element's for all commands
+	 * @return object of Object Class
+	 * @throws CommandException
+	 *             Exception type for command layer
+	 */
 	Object execute(String... request) throws CommandException;
 }
--- src/main/java/com/epam/library/command/interfaces/package-info.java
+++ src/main/java/com/epam/library/command/interfaces/package-info.java
@@ -1,8 +1,7 @@
 /**
- * 
- */
-/**
- * @author Eugene13
- *
+ * Contains {@link com.epam.library.command.interfaces.Command} interface.
+ * <p>
+ * <i>All Beans classes implement interface Serializable and override methods
+ * equals, hashCode.</i>
  */
 package com.epam.library.command.interfaces;
