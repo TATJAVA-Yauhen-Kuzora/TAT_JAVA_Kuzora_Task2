496964551db642946bea3e6dd18acba6c0239b57
Yauhen Kuzora <Yauhen_Kuzora@epam.com>
2017-07-20 17:46:17 +0300

    fix

--- src/main/java/com/epam/library/guicontroller/GuiController.java
+++ src/main/java/com/epam/library/guicontroller/GuiController.java
@@ -1,7 +1,6 @@
 package com.epam.library.guicontroller;
 
 import java.util.ArrayList;
-
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
